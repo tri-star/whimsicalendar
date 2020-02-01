e53670a8aba1e688722ab697f4f9082d2f899ec7
tristar <tristar@urban-theory.net>
2020-02-01 18:47:32 +0900

    暫定的にトップケージを表示するまでの実装を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:whimsicalendar/pages/top_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  static FirebaseAnalytics analytics = FirebaseAnalytics();
-  static FirebaseAnalyticsObserver observer =
-      FirebaseAnalyticsObserver(analytics: analytics);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -2,7 +2,28 @@ import 'package:flutter/material.dart';
 
 class TopPage extends StatelessWidget {
   @override
+  @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(title: Text('Whisimicalendar')),
+      body: Column(children: [CalendarSection()]),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () => {}, child: Icon(Icons.add)),
+    );
+  }
+}
+
+class CalendarSection extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CalendarSectionState();
+}
+
+class CalendarSectionState extends State<CalendarSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: ListView(
+      children: [Text('ここにカレンダー表示')],
+    ));
   }
 }

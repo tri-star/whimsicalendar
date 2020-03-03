02dbce985a38f70978fabb48c52793bdff7aa87f
tristar <tristar@urban-theory.net>
2020-03-03 22:37:34 +0900

    イベントの一覧を返すメソッドをRepositoryに追加

--- lib/domain/calendar/calendar_event_repository_interface.dart
+++ lib/domain/calendar/calendar_event_repository_interface.dart
@@ -1,8 +1,14 @@
 import 'package:whimsicalendar/domain/user/user.dart';
 
+import '../../widgets/calendar/event_collection.dart';
 import 'calendar_event.dart';
 
 /// カレンダーのイベントを扱うリポジトリ
 abstract class CalendarEventRepositoryInterface {
+  /// イベントの情報を保存する
   void save(User user, CalendarEvent e);
+
+  /// 指定した月のイベントの一覧を取得する
+  Future<EventCollection<CalendarEvent>> getCalendarEventByMonth(
+      String userId, int year, int month);
 }

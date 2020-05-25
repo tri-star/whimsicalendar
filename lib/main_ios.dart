48ba32ed5647a22056cc51750602dbadc3a01c74
tristar <tristar@urban-theory.net>
2020-05-26 01:44:58 +0900

    Crashlyticsを追加

--- lib/main_ios.dart
+++ lib/main_ios.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'application.dart';
@@ -5,5 +8,9 @@ import 'application.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(Application());
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  runZoned<Future<void>>(() async {
+    runApp(Application());
+  }, onError: Crashlytics.instance.recordError);
 }

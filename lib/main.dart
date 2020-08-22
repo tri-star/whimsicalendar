2f6e2817c0af46a62d1b6fc0ee6b68e9d3e6214b
tristar <tristar@urban-theory.net>
2020-08-22 09:43:26 +0900

    以下を参考にFirebaseのアップデートに対応。 https://firebase.flutter.dev/docs/migration/

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,7 @@ import 'application.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp();
   Crashlytics.instance.enableInDevMode = true;
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
   runZoned<Future<void>>(() async {

83595e5404316fd910795b93b34572b2c9b11642
tristar <tristar@urban-theory.net>
2020-11-09 23:14:54 +0900

    テスト用に一時的にエラーが起きる状態に変更

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -70,20 +70,20 @@ class TopPage extends StatelessWidget {
     //FirebaseCrashlytics.instance.log('test message.');
     //FirebaseCrashlytics.instance.crash();
     try {
-      //throw StateError('test error3');
-      const List list = [];
-      print(list[100]);
-      final registered = await Navigator.of(context).pushNamed('/event/add',
-          arguments: EventRegisterPageArguments(
-              currentDate: Provider.of<CalendarViewModel>(context, listen: false)
-                  .currentDate));
+      throw StateError('test error4');
+      // const List list = [];
+      // print(list[100]);
+      // final registered = await Navigator.of(context).pushNamed('/event/add',
+      //     arguments: EventRegisterPageArguments(
+      //         currentDate: Provider.of<CalendarViewModel>(context, listen: false)
+      //             .currentDate));
 
-      if (registered == true) {
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text('登録しました。')));
-        CalendarViewModel viewModel =
-            Provider.of<CalendarViewModel>(context, listen: false);
-        viewModel.loadEventList();
-      }
+      // if (registered == true) {
+      //   Scaffold.of(context).showSnackBar(SnackBar(content: Text('登録しました。')));
+      //   CalendarViewModel viewModel =
+      //       Provider.of<CalendarViewModel>(context, listen: false);
+      //   viewModel.loadEventList();
+      // }
     } catch(e, s) {
       FirebaseCrashlytics.instance.recordError(e, s);
     }
